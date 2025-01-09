a90fc9d2581efeb9f0ecbf2ecbf010c243ed763a
BellWin98 <jongseunghan@naver.com>
2025-01-09 13:30:15 +0900

    Initialize

---
+++ src/main/java/com/beboilerplate/BeBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.beboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBoilerPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeBoilerPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beboilerplate/BeBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBoilerPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
